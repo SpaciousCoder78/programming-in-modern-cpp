23ecc9e72e4ac61936165a5048b44674e4e0541d
Aryan Karamtoth <88923986+SpaciousCoder78@users.noreply.github.com>
2024-07-18 15:50:01 +0530

    Create 04.02 - using sort from stl.cpp

---
+++ Week 1/Module 4/Programs/04.02 - using sort from stl.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+bool compare(int i,int j) {
+    return(i>j);
+}
+
+int main() {
+    int data[]={32,71,12,45,26};
+    sort(data,data+5,compare);
+    for(int i=0;i<5;i++)
+        cout<<data[i]<<" ";
+}
