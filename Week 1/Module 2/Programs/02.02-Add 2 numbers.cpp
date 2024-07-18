fd8b2a5aaeeeafae61e25175a73ba5df25a07b8e
Aryan Karamtoth <88923986+SpaciousCoder78@users.noreply.github.com>
2024-07-18 11:15:57 +0530

    Create 02.02-Add 2 numbers.cpp

---
+++ Week 1/Module 2/Programs/02.02-Add 2 numbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+   int a,b;
+   cout<<"Input two numbers\n";
+   cin>>a>>b;
+   int sum = a+b;
+   cout<<"Sum of "<<a<<"and "<<b<<"is: "<<sum<<std::endl;
+
+}
