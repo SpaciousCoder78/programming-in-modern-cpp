8ac6993812b0d07d4c356e5668880a45da6c760d
Aryan Karamtoth <88923986+SpaciousCoder78@users.noreply.github.com>
2024-07-18 15:37:47 +0530

    Create 03.05 - Concatenation of strings.cpp

---
+++ Week 1/Module 3/Programs/03.05 - Concatenation of strings.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main() {
+    string str1 = "HELLO";
+    string str2 = "WORLD";
+
+    string str = str1 + str2;
+
+    cout<<str;
+}
