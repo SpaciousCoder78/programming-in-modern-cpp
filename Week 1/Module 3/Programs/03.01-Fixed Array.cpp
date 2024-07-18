158b0df1c8c32ad0e042bff5cd3e7f172a1cd5fd
Aryan Karamtoth <88923986+SpaciousCoder78@users.noreply.github.com>
2024-07-18 15:30:58 +0530

    Create 03.01-Fixed Array.cpp

---
+++ Week 1/Module 3/Programs/03.01-Fixed Array.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    short age[4];
+
+    age[0]=23;
+    age[1]=34;
+    age[2]=65;
+    age[3]=74;
+
+    cout<<age[0]<<" ";
+    cout<<age[1]<<" ";
+    cout<<age[2]<<" ";
+    cout<<age[3]<<" ";
+}
