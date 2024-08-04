5e7efcf211be529690b9f40eff2d7b0bbd715731
Aryan Karamtoth <88923986+SpaciousCoder78@users.noreply.github.com>
2024-08-04 13:14:36 +0530

    Create Programming_Assignment_1.cpp

---
+++ Week 3/Assignments/Programming_Assignment_1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+class Pair {
+    const int *a, *b;
+    public:
+        Pair(int val1, int val2) : a(new int(val1)),b(new int(val2)){}    //LINE-1
+        ~Pair(){delete a; delete b;}    //LINE-2
+        int getA() {  return *a; }    //LINE-3
+        int getB() { return *b; }    //LINE-4
+};
+
+int main(){
+    int x, y;
+    cin >> x >> y;
+    Pair p(x, y);
+    cout << "[" << p.getA() << ", " << p.getB() << "]";
+    return 0;
+}
